5746bb77c99b793b6a445263cf7e64c61510056d
gujev <gujevinho@gmail.com>
2026-02-06 15:01:47 -0300

    Ajuste saml_utils alterar cert_dir para CERT

--- saml_utils.py
+++ saml_utils.py
@@ -207,8 +207,8 @@ def validate_user_credentials(username, password):
 
 # Adicione esta função para inicializar o diretório de certificados
 def init_cert_dir():
-    if not os.path.exists(Config.CERT_DIR):
-        os.makedirs(Config.CERT_DIR)
+    if not os.path.exists(Config.CERT):
+        os.makedirs(Config.CERT)
 # E chame-a, por exemplo, no config.py ou aqui
 init_cert_dir()
 
